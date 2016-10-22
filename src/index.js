c8081be63ee26de37361a6fc689994e6a0bacb8f
Dedi Suhanda <dedi.suhanda@gmail.com>
2016-10-23 01:45:40 +0700

    feat(random): Add ability to get and array of names
    if you pass a number to the random function , you will recive an array with that number of rantom
    
    items

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 var randomName = require('./random-name.json');
 var uniqueRandomArray = require('unique-random-array');
+var getRandomName = uniqueRandomArray(randomName);
 
 module.exports = {
     all: randomName,
-    random: uniqueRandomArray(randomName)
+    random: function (number) {
+        if (number === undefined) {
+            return getRandomName();
+        }
+        var randomItems = [];
+        for (var i = 0; i < number; i++) {
+            randomItems.push(getRandomName());
+        }
+        return randomItems;
+    }
 }
--- src/index.test.js
+++ src/index.test.js
@@ -25,8 +25,16 @@ describe('random name', function () {
     describe('random', function () {
         it('should include a random name from random-name.json', function () {
             var randomName = randomNames.random();
-
-            expect(randomName).to.be.include(randomName);
+            expect(randomNames.all).to.be.include(randomName);
         });
+
+        it('should return an array of random items if passed a number', function () {
+            var randomItems = randomNames.random(3);
+            expect(randomItems).to.have.length(3);
+
+            randomItems.forEach(function (item) {
+                expect(randomNames.all).to.be.include(item);
+            });
+        })
     });
 });
